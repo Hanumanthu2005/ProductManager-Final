2ee6ad59d9f78303d7db8893afea38bcac85f3ef
hanumantharaovaddi <hanumanthurao9381@gmail.com>
2025-07-03 10:56:03 +0530

    init commit

--- src/main/java/com/hanu/ProductManagerSpringWeb/ProductController.java
+++ src/main/java/com/hanu/ProductManagerSpringWeb/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class ProductController {
 
+    //controller
     @Autowired
     ProductService service;
     @RequestMapping("/")
